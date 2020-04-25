6343b06d9f33ba12a6e37622fb2346f45d8521b1
alexanalyticsander <58505988+alexanalyticsander@users.noreply.github.com>
2020-04-25 15:25:48 -0700

    Update and rename Q4a.java to degreediff.java

--- degreediff.java
+++ degreediff.java
@@ -11,54 +11,46 @@ import java.io.IOException;
 
 public class Q4a {
 
-    final String gtid = "asong49";
+final String gtid = "asong49";
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
 
-    Text node = new Text();
-    IntWritable plus = new IntWritable(1);
-    IntWritable minus = new IntWritable(-1);
+	Text node = new Text();
+	IntWritable plus = new IntWritable(1);
+	IntWritable minus = new IntWritable(-1);
 	
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-String line = value.toString();
-String[] fields = line.split("\t");
-
-String node1 = String.valueOf(fields[0]);
-node.set(node1);
-context.write(node, plus);
-String node2 = String.valueOf(fields[1]);
-node.set(node2);
-context.write(node, minus);
-
+	String line = value.toString();
+	String[] fields = line.split("\t");
+	String node1 = String.valueOf(fields[0]);
+	node.set(node1);
+	context.write(node, plus);
+	String node2 = String.valueOf(fields[1]);
+	node.set(node2);
+	context.write(node, minus);
 }
 }
 
-public static class DegreeMapper extends Mapper<Object, Text, Text, IntWritable>{
-private IntWritable degree = new IntWritable(1);
-private Text freq = new Text();
+	public static class DegreeMapper extends Mapper<Object, Text, Text, IntWritable>{
+	private IntWritable degree = new IntWritable(1);
+	private Text freq = new Text();
 
-public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-String [] lines = value.toString().split("\t");
-freq.set(lines[1]);
-context.write(freq, degree);
-
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+	String [] lines = value.toString().split("\t");
+	freq.set(lines[1]);
+	context.write(freq, degree);
 }
 }
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
+	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 	IntWritable result = new IntWritable();    
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 	int sum = 0;
-      for (IntWritable value : values) {
-      sum = sum + value.get();
-    }
+	for (IntWritable value : values) {
+	sum = sum + value.get();
+}
       result.set(sum);
       context.write(key, result);
 }
@@ -92,5 +84,5 @@ context.write(freq, degree);
     FileOutputFormat.setOutputPath(job2, new Path(args[1]));
     System.exit(job2.waitForCompletion(true) ? 0 : 1);
 
-  }
+}
 }
