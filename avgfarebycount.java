bc254358aff46c3fa9e3ed00fe82054895788440
alexanalyticsander <58505988+alexanalyticsander@users.noreply.github.com>
2020-04-25 15:27:37 -0700

    Update and rename Q4b.java to avgfarebycount.java

--- avgfarebycount.java
+++ avgfarebycount.java
@@ -11,53 +11,48 @@ import java.io.IOException;
 
 public class Q4b {
 
-    final String gtid = "asong49";
+	final String gtid = "asong49";
 
-public static class TokenizerMapper
-       extends Mapper<Object, Text, IntWritable, Text>{
+	public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, Text>{
 
-    IntWritable intkey = new IntWritable();
-    Text textval = new Text();
-    IntWritable one = new IntWritable(1);
+	IntWritable intkey = new IntWritable();
+	Text textval = new Text();
+	IntWritable one = new IntWritable(1);
 	
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-String line = value.toString();
-String[] fields = line.split("\t");
-int passenger = Integer.parseInt(fields[2]);
-double v = Double.parseDouble(fields[3]);
-String h = String.valueOf(one) + "," + String.valueOf(v);
-intkey.set(passenger);
-textval.set(h);
-context.write(intkey, textval);
+	String line = value.toString();
+	String[] fields = line.split("\t");
+	int passenger = Integer.parseInt(fields[2]);
+	double v = Double.parseDouble(fields[3]);
+	String h = String.valueOf(one) + "," + String.valueOf(v);
+	intkey.set(passenger);
+	textval.set(h);
+	context.write(intkey, textval);
 
 }
 }
 
-  public static class IntSumReducer
-       extends Reducer<IntWritable,Text,IntWritable,Text> {
+	public static class IntSumReducer extends Reducer<IntWritable,Text,IntWritable,Text> {
 	Text val = new Text();
 	FloatWritable result = new FloatWritable(); 
 
 
-    public void reduce(IntWritable key, Iterable<Text> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
+	public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 	int total = 0;
 	double sum = 0;
 	double avg = 0;
-      for (Text value : values) {
+	for (Text value : values) {
         String line = value.toString();
 	String[] field = line.split(",");
 	total = total + Integer.parseInt(field[0]);
 	sum = sum + Double.parseDouble(field[1]);
         avg = sum/total;
 
-    }
+}
 	String z = String.format("%.2f", avg);
 	val.set(z);
-      context.write(key, val);
+	context.write(key, val);
 }
 }
 
